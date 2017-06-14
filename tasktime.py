2c7d9b7ba14a820f5cf2d2c9171938e652b9f131
Lyz <lyz@riseup.net>
2017-06-14 09:35:02 +0200

    * [fix] change custom argument parser to argparse

--- tasktime.py
+++ tasktime.py
@@ -2,15 +2,13 @@
 
 # Copyright (c) 2012 Sven Hertle <sven.hertle@googlemail.com>
 
-import sys
-import subprocess
-import json
 import re
+import sys
+import json
 import datetime
+import argparse
+import subprocess
 
-#
-# Calculations
-#
 
 class Calculator:
     printer = None
@@ -27,31 +25,32 @@ class Calculator:
 
     def setTaskCmd(self, task_cmd):
         self.task_cmd = task_cmd
-    
+
     def setPrintNull(self, print_null):
         self.print_null = print_null
 
     def create_statistic(self, project):
-        if self.printer == None:
+        if self.printer is None:
             print("Printer is None")
             sys.exit(1)
 
         # Get data from taskwarrior
         try:
-            json_tmp = subprocess.check_output([self.task_cmd, 
-                                                "export", 
+            json_tmp = subprocess.check_output([self.task_cmd,
+                                                "export",
                                                 "pro:" + project,
-                                                "rc.json.array=on",
-                                               ])
+                                                "rc.json.array=on"
+                                                ])
         except OSError as e:
             print(str(e))
             sys.exit(1)
         except subprocess.CalledProcessError as e:
-            print("Export from taskwarrior fails: " + str(e.output, encoding="utf8"))
+            print("Export from taskwarrior fails: {}".format(
+                str(e.output, encoding="utf8")))
             sys.exit(1)
 
         # Make valid JSON
-        json_str=str(json_tmp, encoding="utf8")
+        json_str = str(json_tmp, encoding="utf8")
 
         # Parse JSON
         tasks = json.loads(json_str)
@@ -95,32 +94,30 @@ class Calculator:
         return delta.total_seconds()
 
     def internal_to_datetime(self, string):
-        match = re.search("^(\d{4})(\d{2})(\d{2})T(\d{2})(\d{2})(\d{2})Z$", string)
+        match = re.search(
+            "^(\d{4})(\d{2})(\d{2})T(\d{2})(\d{2})(\d{2})Z$", string)
 
-        if match == None:
+        if match is None:
             return None
 
         year = int(match.group(1))
         month = int(match.group(2))
         day = int(match.group(3))
-        
+
         hour = int(match.group(4))
         minute = int(match.group(5))
         second = int(match.group(6))
 
         return datetime.datetime(year, month, day, hour, minute, second)
 
-#
-# Printer
-#
 
 class Printer:
     def print_header(self, project):
         raise NotImplementedError()
-    
+
     def print_task(self, description, seconds):
         raise NotImplementedError()
-    
+
     def print_result(self, seconds):
         raise NotImplementedError()
 
@@ -129,12 +126,15 @@ class Printer:
         minute = (seconds // 60) % 60
         hour = (seconds // 3600)
 
-        return self._number_to_2_digits(hour) + ":" + self._number_to_2_digits(minute) + ":" + self._number_to_2_digits(second)
+        return "{}:{}:{}".format(
+            self._number_to_2_digits(hour),
+            self._number_to_2_digits(minute),
+            self._number_to_2_digits(second))
 
     def _number_to_2_digits(self, n):
         return repr(round(n)).zfill(2)
 
-# CSV
+
 class CSVPrinter(Printer):
     def _csv_encode(self, string):
         return string.replace("\"", "\"\"")
@@ -146,13 +146,14 @@ class CSVPrinter(Printer):
         print("\"\",\"\"")
 
     def print_task(self, description, seconds):
-        print("\"" + self._csv_encode(description) + "\",\"" + self.seconds_to_readable(seconds) + "\"")
+        print("\"{}\",\"{}\"".format(
+            self._csv_encode(description), self.seconds_to_readable(seconds)))
 
     def print_result(self, seconds):
         print("\"\",\"\"")
         print("\"Sum\",\"" + self.seconds_to_readable(seconds) + "\"")
 
-# Readable
+
 class ReadablePrinter(Printer):
     def print_header(self, project):
         print("Project: " + project)
@@ -167,53 +168,34 @@ class ReadablePrinter(Printer):
         print()
         print("Sum: " + self.seconds_to_readable(seconds))
 
-# Help
-def print_help():
-    print(sys.argv[0] + " [parameters...] <project>")
-    print()
-    print("Calculate and print spent time for a project from taskwarrior")
-    print()
-    print("Parameters:")
-    print("\t-h, --help\t\tShow this help")
-    print("\t-c, --csv\t\tPrint output in CSV format")
-    print("\t-n, --null\t\tPrint also tasks without time information (default: no)")
-    print("\t-t, --task [cmd]\tChange task command")
 
-#
-# Main
-#
+def load_parser(argv):
+    ''' Configure parser '''
+    # Argparse
+    description = "Calculate the spent time for a project from taskwarrior"
+    parser = argparse.ArgumentParser(description=description)
+
+    parser.add_argument("project", type=str, help='Project name')
+    parser.add_argument("-c", "--csv", action="store_true",
+                        help='Print output in CSV format')
+    parser.add_argument("-n", "--null", action="store_true",
+                        help='Print also tasks without time information')
+    parser.add_argument("-tc", "--task_command", type=str, nargs='?',
+                        metavar="cmd", help='Change task command')
+
+    return parser.parse_args()
+
 
 if __name__ == "__main__":
-    params = sys.argv[1:]
-    
+    args = load_parser(sys.argv)
+
     c = Calculator()
 
-    project = None
-    show_help = False
+    if args.csv:
+        c.setPrinter(CSVPrinter())
+    if args.null:
+        c.setPrintNull(True)
+    if args.task_command:
+        c.setTaskCmd(args.task_command)
 
-    skip = False
-    for i,param in enumerate(params):
-        if skip:
-            skip = False
-            continue
-
-        if param == "--csv" or param == "-c":
-            c.setPrinter(CSVPrinter())
-        elif param == "--help" or param == "-h":
-            show_help = True
-        elif param == "--task" or param == "-t":
-            if i == len(params)-1: # Last parameter -> error
-                print("--task needs another parameter")
-                sys.exit(1)
-            else:
-                c.setTaskCmd(params[i+1])
-                skip = True
-        elif param == "--null" or param == "-n":
-            c.setPrintNull(True)
-        elif i == len(params)-1: # Last parameter
-            project = param
-
-    if show_help or project == None:
-        print_help()
-    else:
-        c.create_statistic(project)
+    c.create_statistic(args.project)
